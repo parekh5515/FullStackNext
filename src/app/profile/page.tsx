35f7c3e48a44334c07b263caecef3bc34c50962a
deep-1C <deep22.oneclick@gmail.com>
2025-06-23 14:15:35 +0530

    logout completed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
-export default function ProfilePage(){
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p>Profile Page</p>
-        </div>
-    )
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+export default function ProfilePage() {
+  const roouter = useRouter();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success(response.data.message);
+      roouter.push("/login");
+    } catch (error: any) {
+      console.log("error in logout", error.message);
+      toast.error(error.response.data.error);
+    }
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
